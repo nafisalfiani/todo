0ca6c3eea70ee7a6f3ebc808a6d048579a276734
nafisalfiani <90552224+nafisalfiani@users.noreply.github.com>
2022-03-04 17:30:28 +0700

    Update app.js

--- servers/node/app.js
+++ servers/node/app.js
@@ -7,14 +7,9 @@ if (process.env.NODE_ENV === 'development') {
 const express = require('express');
 const router = require('./routes');
 const config = require('./config/config.js');
-const port = 3000;
 const app = express();
 
 app.use(express.json());
 app.use(router);
-
-app.listen(port, () => {
-    console.log(`Listening on ${port}`);
-});
 
 module.exports = app;
