4037dff9550f2fe8cd8d780da93beb7298de50aa
nafisalfiani <90552224+nafisalfiani@users.noreply.github.com>
2022-03-04 14:17:21 +0700

    Add readme (#3)
    * Caching get all user
    
    * Fix redis cache
    
    Co-authored-by: Nafisa Alfiani <nafisaalfiani@Nafisas-MacBook-Pro.local>

--- servers/orchestrator/app.js
+++ servers/orchestrator/app.js
@@ -8,13 +8,22 @@ const app = express();
 
 app.use(express.json());
 
+// Caching get all user.
+
 app.get('/listOfUser' , async (req , res)=>{
     try {
-        const listOfUser = await axios.get('http://localhost:3000/user/list');
-        const data = listOfUser.data;
-        res.status(200).json(data)
+        const cache = await redis.get('/user/list')
+        if (cache) {
+            res.status(200).json(JSON.parse(cache));
+            
+        } else {
+            const listOfUser = await axios.get('http://localhost:3000/user/list');
+            const data = listOfUser.data;
+            await redis.set('/listOfUser', JSON.stringify(data));
+            res.status(200).json(data);
+        }
     } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
  })
 
@@ -23,9 +32,9 @@ app.get('/listOfUser' , async (req , res)=>{
         let id = req.params.id
         const todo = await axios.get(`http://localhost:8090/todo/${id}/history`);
         const data = todo.data;
-        res.status(200).json(data)
+        res.status(200).json(data);
     } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
  })
 
@@ -35,7 +44,7 @@ app.get('/listOfUser' , async (req , res)=>{
             method: 'post',
             url: `http://localhost:3000/task/create`,
             data: req.body,
-        })
+        });
 
         const todo_history = await axios({
             method: 'post',
@@ -43,8 +52,8 @@ app.get('/listOfUser' , async (req , res)=>{
             data: req.body,
           });
         
-          const data = todo_history.data
-        res.status(200).json(data)
+        const data = todo_history.data;
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json(error)
     }
@@ -52,30 +61,26 @@ app.get('/listOfUser' , async (req , res)=>{
 
  app.put('/task/update/:id' , async (req , res)=>{
     try {
-        let id = req.params.id
+        let id = req.params.id;
 
         const todo = await axios({
             method: 'put',
             url: `http://localhost:3000/task/update/${id}`,
             data: req.body,
-        })
+        });
 
         const todo_history = await axios({
             method: 'post',
             url: `http://localhost:8090/todo/${id}/history`,
             data: req.body,
-          });
+          });;
           
         const data = todo_history.data;
-        res.status(200).json(data)
+        res.status(200).json(data);
     } catch (err) {
-        res.status(500).json(err)
+        res.status(500).json(err);
     }
  })
-
-app.post('/' , (req , res)=>{
-   res.send('hello from simple server :)')
-})
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
